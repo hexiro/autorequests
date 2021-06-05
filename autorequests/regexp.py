c2f0437713059fe521bbb0bb140f61734c0fcd7a
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-06-05 14:48:10 -0400

    update fetch regexp (again I know)

--- autorequests/regexp.py
+++ autorequests/regexp.py
@@ -1,15 +1,15 @@
 import re
 
-# https://regex101.com/r/Y6NqA9/12
+# https://regex101.com/r/Y6NqA9/15
 # ensures general format but not too restrictive with what can be passed
 # basically just allows all possible chars for everything as long as it meets expected format
 # I keep learning that things can be optional,
-# so now everything besides headers is optional
+# so now everything besides headers is optional just in case
 fetch_regexp = re.compile(
     r"^fetch\(\"(?P<url>(?:http|https):\/\/.+)\", {\n  \"headers\": (?P<headers>{(?:.|\n)+}),\n"
     r"(?:  \"referrer\": \"(?P<referrer>.+)\",\n|)"
     r"(?:  \"referrerPolicy\": \"(?P<referrer_policy>.+)\",\n|)"
-    r"(?:  \"body\": \"(?P<body>.+)\",\n|)"
+    r"(?:  \"body\": (?:\"|)(?P<body>.+?)(?:\"|),\n|)"
     r"(?:  \"method\": \"(?P<method>[A-Z]+)\",\n|)"
     r"(?:  \"[a-z]+\": \".+\"(?:,|)\n|)*}\);$"
 )
