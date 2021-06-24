de7e40d5f500d35d789aa20dddf1ea040bc503dd
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-06-23 20:47:25 -0400

    update repr + comments in `Parameter`

--- autorequests/classes/parameter.py
+++ autorequests/classes/parameter.py
@@ -1,15 +1,17 @@
 class Parameter:
 
     def __init__(self, name: str, typehint: type = None, default: str = None):
-        # default=None literally means there is no default
-        # not that the default value is `None`
+        # isn't really used to it's full potential right now
+        # we'll have to see if a good way to implement custom parameters is found
         self.__name = name
         # resolves <class 'str'> to str
         self.__typehint = typehint.__name__ if typehint else None
+        # default=None literally means there is no default
+        # not that the default value is `None`
         self.__default = f"\"{default}\"" if default else None
 
     def __repr__(self):
-        return f"<{self.code()}>"
+        return f"<Parameter {self.code()}>"
 
     def code(self):
         if self.typehint and self.default:
