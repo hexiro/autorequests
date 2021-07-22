1a30824ca1644206b3eca4eee2a96e2cb310314b
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-07-22 16:17:44 -0400

    rework `Case` class w/ `@cached_property`-s

--- autorequests/classes/case.py
+++ autorequests/classes/case.py
@@ -1,7 +1,7 @@
 import string
 
 from ..regexp import fix_snake_case_regexp
-from ..utils import uses_accepted_chars
+from ..utils import uses_accepted_chars, cached_property
 
 
 class Case:
@@ -14,38 +14,32 @@ class Case:
 
     def __init__(self, text: str):
         self.__text = text
-        if self.is_no_case:
-            snaked_text = self.text
-        elif self.is_kebab_case:
-            snaked_text = self.kebab_to_snake(self.text)
-        elif self.is_dot_case:
-            snaked_text = self.dot_to_snake(self.text)
-        elif self.is_camel_case:
-            snaked_text = self.camel_to_snake(self.text)
-        elif self.is_pascal_case:
-            snaked_text = self.pascal_to_snake(self.text)
-        else:
-            # attempt to parse text
-            snaked_text = self.text
-            snaked_text = self.kebab_to_snake(snaked_text)
-            snaked_text = self.dot_to_snake(snaked_text)
-            snaked_text = self.camel_to_snake(snaked_text)
-            # pascal not really needed
-
-        snaked_text = fix_snake_case_regexp.sub("_", snaked_text)
-        # it hurts my eyes less not seeing these ugly self.__s everywhere
-        # so i define it first w/o self
-        self.__snaked_text = snaked_text
 
     @property
     def text(self):
         return self.__text
 
-    @property
+    @cached_property
     def snake_case(self):
-        return self.__snaked_text
+        if self.is_no_case:
+            return self.text
+        if self.is_kebab_case:
+            return self.kebab_to_snake(self.text)
+        if self.is_dot_case:
+            return self.dot_to_snake(self.text)
+        if self.is_camel_case:
+            return self.camel_to_snake(self.text)
+        if self.is_pascal_case:
+            return self.pascal_to_snake(self.text)
+        # attempt to parse text
+        snaked_text = self.text
+        snaked_text = self.kebab_to_snake(snaked_text)
+        snaked_text = self.dot_to_snake(snaked_text)
+        snaked_text = self.camel_to_snake(snaked_text)
+        # pascal -> snake and camel -> snake are the same function, so pascal isn't needed
+        return fix_snake_case_regexp.sub("_", snaked_text)
 
-    @property
+    @cached_property
     def is_no_case(self) -> bool:
         """
         :returns: true if text is a single lowercase word
@@ -53,19 +47,19 @@ class Case:
         """
         return self.text.islower() and uses_accepted_chars(self.text, string.ascii_lowercase)
 
-    @property
+    @cached_property
     def is_snake_case(self) -> bool:
         return self.text.islower() and uses_accepted_chars(self.text, self.snake_case_chars)
 
-    @property
+    @cached_property
     def is_kebab_case(self) -> bool:
         return self.text.islower() and uses_accepted_chars(self.text, self.kebab_case_chars)
 
-    @property
+    @cached_property
     def is_dot_case(self) -> bool:
         return self.text.islower() and uses_accepted_chars(self.text, self.dot_case_chars)
 
-    @property
+    @cached_property
     def is_camel_case(self) -> bool:
         return self.text[0].islower() and not self.text.islower() and \
                uses_accepted_chars(self.text, self.camel_case_chars)
