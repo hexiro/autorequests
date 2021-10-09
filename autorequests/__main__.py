1b4c78a2fb5894639be44da5aac9b63d04592a67
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-10-09 16:41:04 -0400

    refactor: move method parsing to new file

--- autorequests/__main__.py
+++ autorequests/__main__.py
@@ -1,13 +1,12 @@
 import argparse
-import json
 from pathlib import Path
 from typing import List, Optional, Dict, Generator
 
 import rich
 
-from . import regexp
-from .classes import Class, Method, URL, Body
-from .utils import cached_property, extract_cookies
+from .parsing import method_from_text
+from .classes import Class, Method
+from .utils import cached_property
 
 __version__ = "1.0.2"
 __all__ = (
@@ -124,7 +123,7 @@ class AutoRequests:
         methods = []
         for file in self.files_from_path(path):
             text = file.read_text(encoding="utf8", errors="ignore")
-            method = self.method(text)
+            method = method_from_text(text)
             if method is None:
                 continue
             methods.append(method)
@@ -157,102 +156,6 @@ class AutoRequests:
 
     def find_class(self, name: str) -> Optional[Class]:
         return next((cls for cls in self.classes if cls.name == name), None)
-
-    @classmethod
-    def method(cls, text: str) -> Optional[Method]:
-        # short circuiting
-        if text:
-            return cls.method_from_fetch(text) or cls.method_from_powershell(text)
-
-    @staticmethod
-    def method_from_fetch(text: str) -> Optional[Method]:
-        """
-        Parses a file that follows this format:
-        (with some being optional)
-
-        fetch(<URL>, {
-          "headers": <HEADERS>,
-          "referrer": <REFERRER>,
-          "referrerPolicy": <REFERRER-POLICY>,
-          "body": <BODY>,
-          "method": <METHOD>,
-          "mode": <MODE>
-        });
-        """
-        fetch = regexp.fetch_regexp.search(text)
-        if not fetch:
-            return
-
-        headers = json.loads(fetch["headers"])
-        # referer is spelled wrong in the HTTP header
-        # referrer policy is not
-        referrer = fetch["referrer"]
-        referrer_policy = fetch["referrer_policy"]
-        if referrer:
-            headers["referer"] = referrer
-        if referrer_policy:
-            headers["referrer-policy"] = referrer_policy
-
-        cookies = extract_cookies(headers)
-
-        method = fetch["method"]
-        url = URL(fetch["url"])
-        body = Body(fetch["body"])
-
-        return Method(method=method,
-                      url=url,
-                      body=body,
-                      headers=headers,
-                      cookies=cookies,
-                      )
-
-    @staticmethod
-    def method_from_powershell(text: str) -> Optional[Method]:
-        """
-        Parses a file that follows this format:
-        (with some potentially being optional)
-
-        Invoke-WebRequest -Uri <URL> `
-        -Method <METHOD> `   # optional; defaults to GET if not set
-        -Headers <HEADERS> `
-        -ContentType <CONTENT-TYPE> `   # optional; only exists w/ body
-        -Body <BODY>     # optional; only exists if a body is present
-        """
-        powershell = regexp.powershell_regexp.search(text)
-        if not powershell:
-            return
-
-        headers = {}
-        for header in powershell["headers"].splitlines():
-            if "=" in header:
-                header = header.lstrip("  ")
-                key, value = header.split("=", maxsplit=1)
-                # remove leading and trailing "s that always exist
-                key = key[1:-1]
-                value = value[1:-1]
-                headers[key] = value
-
-        cookies = extract_cookies(headers)
-
-        # ` is the escape character in powershell
-        # replace two `s with a singular `
-        # replace singular `s with nothing
-
-        raw_body = powershell["body"]
-        if raw_body:
-            raw_body = raw_body.split("`")
-            raw_body = "".join((e if e != "" else "`") for e in raw_body)
-
-        method = powershell["method"] or "GET"
-        url = URL(powershell["url"])
-        body = Body(raw_body)
-
-        return Method(method=method,
-                      url=url,
-                      body=body,
-                      headers=headers,
-                      cookies=cookies,
-                      )
 
     def main(self):
         self.write()
