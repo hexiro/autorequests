80ac492d1840e1e989994ce68ba020db67fe3f4e
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-10-20 18:38:00 -0400

    refactor: parse powershell without regexp

--- autorequests/utilities/parsing.py
+++ autorequests/utilities/parsing.py
@@ -1,8 +1,7 @@
 import json
-from typing import Optional
+from typing import Optional, Dict, List
 
 from . import extract_cookies
-from . import regexp
 from ..classes import Method, URL, Body
 
 
@@ -79,37 +78,83 @@ def method_from_powershell(text: str) -> Optional[Method]:
     -ContentType <CONTENT-TYPE> `   # optional; only exists w/ body
     -Body <BODY>     # optional; only exists if a body is present
     """
-    powershell = regexp.powershell_regexp.search(text)
-    if not powershell:
-        return  # type: ignore
+    headers: Dict[str, str] = {}
+    cookies: Dict[str, str] = {}
+    method: str = "GET"
+    url: Optional[URL]
+    body: Optional[Body]
 
-    headers = {}
-    for header in powershell["headers"].splitlines():
-        if "=" in header:
-            header = header.lstrip("  ")
-            key, value = header.split("=", maxsplit=1)
-            # remove leading and trailing "s that always exist
-            key = key[1:-1]
-            value = value[1:-1]
-            headers[key] = value
+    # parse escape character, `
+    text = text.replace("`", "\\")
+    lines: List[str] = [e.rstrip("\\") for e in text.splitlines()]
 
-    cookies = extract_cookies(headers)
+    # parse custom session
+    parse_session(cookies, headers, lines)
 
-    # ` is the escape character in powershell
-    # replace two `s with a singular `
-    # replace singular `s with nothing
+    # parse arguments
+    args = parse_args("".join(lines))
+    url = URL(args["Uri"])
+    body = Body(args.get("Body"))
 
-    raw_body = powershell["body"]
-    if raw_body:
-        raw_body = "".join((e if e != "" else "`") for e in raw_body.split("`"))
-
-    method = powershell["method"] or "GET"
-    url = URL(powershell["url"])
-    body = Body(raw_body)
+    parse_headers(args, headers)
+    headers.pop("method", None)
 
     return Method(method=method,
                   url=url,
-                  body=body,
                   headers=headers,
                   cookies=cookies,
+                  body=body
                   )
+
+
+def parse_headers(args, headers):
+    headers_string = args["Headers"][3:-2]
+    for header in headers_string.split("\" \""):
+        key, value = header.split("\"=\"", maxsplit=1)
+        headers[key] = value
+
+
+def parse_session(cookies, headers, lines):
+    while lines[0].startswith("$session"):
+        line = lines.pop(0)
+        if line.startswith("$session.UserAgent"):
+            # $session.UserAgent = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en) AppleWebKit (KHTML, like Gecko)"
+            headers["User-Agent"] = line.split("\"")[1]
+        elif line.startswith("$session.Cookies.Add"):
+            # $session.Cookies.Add((New-Object System.Net.Cookie("hello-from", "autorequests", "/", "httpbin.org")))
+            # System.Net.Cookie("hello-from", "autorequests", "/", "httpbin.org")
+            #                       Name         Value       Path     Domain
+            # reference: https://docs.microsoft.com/en-us/dotnet/api/system.net.cookie?view=net-5.0#constructors
+            # path and domain will be ignored because that logic is handled elsewhere
+            left_paren = line.rfind("(") + 1
+            right_paren = line.find(")")
+            # ["hello-from", "autorequests", "/", "httpbin.org"]
+            strings = [x.strip("\"") for x in line[left_paren:right_paren].split(", ")]
+            name, value = strings[:2]
+            cookies[name] = value
+
+
+def parse_args(line: str):
+    args: Dict[str, str] = {}
+
+    line_split: List[str] = line.split()
+
+    def last_key() -> str:
+        return list(args.keys())[-1]
+
+    while line_split:
+        line = line_split.pop(0)
+        if line.startswith("-"):
+            arg = line.lstrip("-")
+            args[arg] = ""
+        elif args and not line.isspace():
+            key = last_key()
+            args[key] = f"{args[key]} {line}"
+
+    for key, value in args.items():
+        value = value.lstrip()
+        if value.startswith("\"") and value.endswith("\""):
+            value = value[1:-1]
+        args[key] = value
+
+    return args
--- autorequests/utilities/regexp.py
+++ autorequests/utilities/regexp.py
@@ -1,13 +1,4 @@
 import re
 
-powershell_regexp = re.compile(
-    r"^Invoke-WebRequest -Uri \""
-    r"(?P<url>(?:http|https):\/\/.+?)\"(?: `\n-Method \""
-    r"(?P<method>[A-Z]+)\"|)(?:(?: | `\n)-Headers @{(?:\n\"method\"=\"[A-Z]+\"|)\n"
-    r"(?P<headers>(?:(?:  |)\".+?\"=\".+?\"\n)*)}|)(?: `\n-ContentType \""
-    r"(?P<content_type>.+)\"|)(?: `\n-Body \""
-    r"(?P<body>.+)\"|)$"
-)
-
 fix_snake_case_regexp = re.compile("_{2,}")
 leading_integer_regexp = re.compile("^[0-9]+")
