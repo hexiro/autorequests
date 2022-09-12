b67b98752a64d8c397718c45b06491686c979511
Hexiro <42787085+Hexiro@users.noreply.github.com>
2022-09-12 10:30:29 -0400

    fix: .gitignore

---
+++ tests/examples/httpbin_examples.py
@@ -0,0 +1,10 @@
+httpbin_example_one = """fetch("http://httpbin.org/ip", {
+  "headers": {
+    "accept": "application/json",
+    "accept-language": "en-US,en;q=0.9",
+    "Referer": "http://httpbin.org/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": null,
+  "method": "GET"
+});"""
